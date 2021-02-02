6acdb2fdf6bd48360b6a63743772c59817244602
Richard Busby <rsbusby@gmail.com>
2021-02-01 21:19:33 -0800

    adjusted ripples

--- harp.js
+++ harp.js
@@ -55,11 +55,17 @@ function draw(){
             line(x1=lines[i].x1, y1=lines[i].y1, x2=lines[i].x2, y2=lines[i].y2);
             
             if (lines[i].ripple){
-              var cur_ripple = lines[i].ripple;
-              strokeWeight(cur_ripple.weight);
-              ellipse(cur_ripple.x, cur_ripple.y, cur_ripple.radius)
-              cur_ripple.radius += 1;
               
+              var cur_ripple = lines[i].ripple;
+
+              // width of ripple line
+              strokeWeight(cur_ripple.radius / 24 );
+              noFill();
+              ellipse(cur_ripple.x, cur_ripple.y, cur_ripple.radius);
+              // fill();
+
+              // speed of ripple expansion
+              cur_ripple.radius += 20;
             }
             
             lines[i].fade();
@@ -70,7 +76,7 @@ function draw(){
             
           } 
           for (var i=0; i<lines.length; i++) {
-              if (lines[i].opacity < 0.05) {
+              if (lines[i].opacity < 0.01) {
                   console.log('removing ' + i + ' from ' + lines.length);
                   lines[i].synth.triggerRelease();
                   var old_line = lines[i];
@@ -82,8 +88,6 @@ function draw(){
 
               }
           }
-
-
     }
 }
 
@@ -124,7 +128,7 @@ function perp_line(x1, y1, x2, y2){
     var dist_new = dist(x1, y1, x2, y2);
   
     if (dist_new == 0){
-      weight = 50;
+      weight = 0;
     }
     else{
        weight = 1 + dist_new * 0.1;
@@ -210,7 +214,7 @@ function play_line(x1, y1, x2, y2){
 function Ripple(x, y, weight=20) {
   this.x = x;
   this.y = y;
-  this.radius = 4;
+  this.radius = 50;
   this.weight = weight;
 }
 
